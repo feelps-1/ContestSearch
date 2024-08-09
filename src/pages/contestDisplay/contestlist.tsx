2d95be12aa7874cf03b7151c7b7f5d8caab730f4
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-08-08 21:59:50 -0300

    Correção no tamanho dos cards

--- src/pages/contestDisplay/contestlist.tsx
+++ src/pages/contestDisplay/contestlist.tsx
@@ -2,7 +2,7 @@ import { ContestCard } from "../../components/contextcard";
 
 export function ContestList() {
     return (
-        <div className="flex flex-col w-6/12 items-center gap-3 justify-center">
+        <div className="flex flex-col w-8/12 items-center gap-3 justify-center">
             <div className="w-full h-[500px] overflow-y-auto flex items-center flex-col gap-3 custom-scrollbar">
                 <ContestCard />
                 <ContestCard />
