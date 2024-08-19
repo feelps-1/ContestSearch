b996bae0f6e00e454287580611c103f937888c93
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-08-18 21:18:38 -0300

    Correção de erros na conexão com API

--- src/pages/contestDisplay/contestlist.tsx
+++ src/pages/contestDisplay/contestlist.tsx
@@ -1,41 +1,49 @@
-import { useParams } from "react-router-dom";
-import { ContestCard } from "../../components/contextcard";
-import { useEffect, useState } from "react";
-import { api } from "../../libs/axios";
+import { useEffect, useState } from 'react';
+import { api } from '../../libs/axios';
+import { ContestCard } from '../../components/contextcard';
 
 interface Contest {
-    ID: number;
-    Name: string;
-    Description: string;
-    Icon: string;
-    Type: string;
-    Tematic: string;
-    Education: string;
+    id: string;
+    name: string;
+    description: string;
+    icon: string;
+    tematic: string;
+    education: string;
+    modality: string;
 }
 
 export function ContestList() {
-    const { contestId } = useParams()
-    const [contests, setContests] = useState<Contest[]>([])
+    const [contests, setContests] = useState<Contest[]>([]);
 
     useEffect(() => {
-        api.get(`/contest/`).then(response => setContests(response.data))
-    }, [contestId])
+        api.get(`/contest`).then(response => {
+            const contestsData = response.data.data.map((item: any) => ({
+                id: item.attributes.id,
+                name: item.attributes.name,
+                description: item.attributes.description,
+                icon: item.attributes.icon,
+                tematic: item.attributes.tematic,
+                education: item.attributes.education,
+                modality: item.attributes.modality
+            }));
+            setContests(contestsData);
+            console.log(response); // Log dos dados recebidos
+        });
+    }, []);
 
     return (
         <div className="flex flex-col w-8/12 items-center gap-3 justify-center">
             <div className="w-full h-[500px] overflow-y-auto flex items-center flex-col gap-3 custom-scrollbar">
                 {contests.map((contest, index) => (
-                    <ContestCard 
-                        key={index}
-                        name={contest.Name} 
-                        icon={contest.Icon}
-                        description={contest.Description}
-                        education={contest.Education}
-                        modality={contest.Type}
-                        tematic={contest.Tematic}
-                    />
+                    <ContestCard key={index}
+                    name = {contest.name}
+                    description={contest.description}
+                    icon={contest.icon}
+                    tematic={contest.tematic}
+                    modality={contest.modality}
+                    education={contest.education}/>
                 ))}
             </div>
         </div>
-    )
+    );
 }
