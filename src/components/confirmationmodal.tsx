b7c93bab650f1a1d6e63cff5d05d1859975b66b1
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-09-21 17:55:23 -0300

    redeploy

--- src/components/confirmationmodal.tsx
+++ src/components/confirmationmodal.tsx
@@ -13,8 +13,8 @@ export function ConfirmationModal() {
 
             <div className='w-[200px] md:w-[400px] rounded-xl py-5 px-6 shadow-shape bg-slate-600 space-y-5 fixed'>
             <div className='space-y-2'>
-            <div className='flex  flex-col items-center justify-between'>
-              <h2 className='text-lg  text-slate-200 font-semibold'>Concurso enviado, muito obrigado!</h2>
+            <div className='flex flex-col items-center justify-between'>
+              <h2 className='text-lg text-slate-200 font-semibold'>Concurso enviado, muito obrigado!</h2>
             </div>
         </div>
         </div>
