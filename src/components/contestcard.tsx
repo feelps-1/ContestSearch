8ef5ce00e26979e772fe4fecbcbe75f3a3986108
feelps-1 <98993176+feelps-1@users.noreply.github.com>
2024-09-21 16:07:46 -0300

    fix: contest cards description text now fills the container

--- src/components/contestcard.tsx
+++ src/components/contestcard.tsx
@@ -13,11 +13,11 @@ export function ContestCard({name, description, icon, modality, tematic, educati
         <a href={site} target="_blank" rel="noopener noreferrer" className="w-11/12">
             <div className="bg-slate-200 flex flex-col md:flex-row items-center p-5 rounded-xl gap-2 cursor-pointer hover:bg-slate-300 h-full text-wrap">
                 <img src={icon} className="size-12 md:size-24 rounded-xl"></img>
-                <div className="flex flex-col md:w-96 w-48">
+                <div className="flex flex-col flex-1">
                     <p className="text-slate-600 font-bold text-xl">{name}</p>
                     <p className="text-slate-600 text-xs md:text-sm text-ellipsis">{description}</p>
                 </div>
-                <div className='md:h-4/6 md:w-px h-px w-4/6   bg-slate-300' />
+                <div className='md:h-4/6 md:w-px h-px w-4/6  bg-slate-300' />
                 <div className="flex flex-col">
                     <p className="text-slate-600 text-xs md:text-sm">Categoria: {education}</p>
                     <p className="text-slate-600 text-xs md:text-sm">Tipo: {modality}</p>
